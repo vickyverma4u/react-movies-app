21ea5e1e7a1422828fbf49c6bfbe7154e96cf097
Abhijeet Verma <vickyverma4u@gmail.com>
2021-04-04 10:56:58 +0530

    Added ‘Login’ button on Header

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import Home from "./screens/home/Home";
 
-ReactDOM.render(<div></div>, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/screens/home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Home.css";
+import Header from "../../common/header/Header";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return <Header />;
+  }
+}
+
+export default Home;
